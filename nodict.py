ee36f72550f6d3f59346b441f260bd9b7de1ae01
Leanne Benson <stylistleannerowe@gmail.com>
2020-07-18 19:18:34 -0400

    finished no dict class trying to resolve errors

--- nodict.py
+++ nodict.py
@@ -3,7 +3,7 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = '???'
+__author__ = 'LeanneBenson'
 
 
 class Node:
@@ -16,30 +16,41 @@ class Node:
         return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
-        return self.key == other.key
+        if self.key == other.key:
+            return True
+        else:
+            return False
 
 
 class NoDict:
     def __init__(self, num_buckets=10):
-        self.buckets = None
-        # Your code here
+        self.buckets = [[] for _ in range(num_buckets)]
+        self.size = num_buckets
 
     def __repr__(self):
-        # Your code here
-        return
+        return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
 
-    def add(self, key, value):
-        # Your code here
-        return
+
+    def add(self, key, value=None):
+        new_node = Node(key, value)
+        bucket = self.buckets[new_node.hash % self.size]
+        for key_value in bucket:
+            if key_value == new_node:
+                bucket.remove(key_value)
+                break
+        bucket.append(new_node)
 
     def get(self, key):
-        # Your code here
-        return
+        key_val = Node(key)
+        bucket = self.buckets[key_val.hash % self.size]
+        for each in bucket:
+            if each == key_val:
+                return each.value
+        raise KeyError(f'{key} not found')
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        value = self.get(key)
+        return value
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        self.add(key, value)
