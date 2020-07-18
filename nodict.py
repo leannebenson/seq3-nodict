0909ffc83801d6cd15fd5ae16780d09634fc68b2
Leanne Benson <stylistleannerowe@gmail.com>
2020-07-18 19:33:41 -0400

    needed to add doc string for 1 error left to pass

--- nodict.py
+++ nodict.py
@@ -8,14 +8,17 @@ __author__ = 'LeanneBenson'
 
 class Node:
     def __init__(self, key, value=None):
+        "doc string"
         self.key = key
         self.value = value
         self.hash = hash(self.key)
 
     def __repr__(self):
+        "doc string"
         return f'{self.__class__.__name__}({self.key}, {self.value})'
 
     def __eq__(self, other):
+        "doc string"
         if self.key == other.key:
             return True
         else:
@@ -24,14 +27,17 @@ class Node:
 
 class NoDict:
     def __init__(self, num_buckets=10):
+        "doc string"
         self.buckets = [[] for _ in range(num_buckets)]
         self.size = num_buckets
 
     def __repr__(self):
+        "doc string"
         return '\n'.join([f'{self.__class__.__name__}.{i}:{bucket}' for i, bucket in enumerate(self.buckets)])
 
 
     def add(self, key, value=None):
+        "doc string"
         new_node = Node(key, value)
         bucket = self.buckets[new_node.hash % self.size]
         for key_value in bucket:
@@ -41,6 +47,7 @@ class NoDict:
         bucket.append(new_node)
 
     def get(self, key):
+        "doc string"
         key_val = Node(key)
         bucket = self.buckets[key_val.hash % self.size]
         for each in bucket:
@@ -49,8 +56,10 @@ class NoDict:
         raise KeyError(f'{key} not found')
 
     def __getitem__(self, key):
+        """doc string"""
         value = self.get(key)
         return value
 
     def __setitem__(self, key, value):
+        "doc string"
         self.add(key, value)
